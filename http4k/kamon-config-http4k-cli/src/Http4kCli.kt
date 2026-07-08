7d0a2d4e1f433a73913d58ba944bd57143d8d392
Souvlakia <souvlakia@vaslabs.io>
2026-07-08 17:44:57 +0300

    Add http4k cli

---
+++ http4k/kamon-config-http4k-cli/src/Http4kCli.kt
@@ -0,0 +1,61 @@
+package io.vaslabs.kamon.tapir.http4k.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.Context
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import io.vaslabs.kamon.tapir.http4k.Http4kFormatter
+import org.http4k.contract.ContractRoute
+import java.io.File
+
+class Http4kCli : CliktCommand(name = "kamon-config-http4k-cli") {
+    override fun help(context: Context): String = "Generate Kamon HOCON instrumentation config from http4k ContractRoutes"
+
+    val className by option("-c", "--class", help = "Fully qualified class/object name containing the routes").default("MainKt")
+    val fieldName by option("-f", "--field", help = "Name of the field or method containing/returning the List<ContractRoute>").default("routes")
+    val backend by option("-b", "--backend", help = "The target Kamon instrumentation backend (e.g. netty, undertow, jetty)").default("http")
+    val output by option("-o", "--output", help = "Path to the output file to write the config to (defaults to stdout)")
+
+    override fun run() {
+        val clazz = Class.forName(className)
+        
+        // Try Kotlin object INSTANCE first, otherwise instantiate with default constructor
+        val instance = try {
+            clazz.getField("INSTANCE").get(null)
+        } catch (e: Exception) {
+            try {
+                clazz.getDeclaredConstructor().newInstance()
+            } catch (e: Exception) {
+                null
+            }
+        }
+
+        // Try getting field, then try invoking method
+        val rawResult = try {
+            val field = clazz.getDeclaredField(fieldName)
+            field.isAccessible = true
+            field.get(instance)
+        } catch (e: Exception) {
+            val method = clazz.getDeclaredMethod(fieldName)
+            method.isAccessible = true
+            method.invoke(instance)
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        val routes = rawResult as? List<ContractRoute>
+            ?: throw IllegalArgumentException("Result of '$fieldName' in '$className' is not a List<ContractRoute>")
+
+        val hocon = Http4kFormatter.format(routes, backend)
+
+        val outputPath = output
+        if (outputPath != null) {
+            File(outputPath).writeText(hocon)
+            echo("Successfully wrote Kamon configuration to $outputPath")
+        } else {
+            echo(hocon)
+        }
+    }
+}
+
+fun main(args: Array<String>) = Http4kCli().main(args)
